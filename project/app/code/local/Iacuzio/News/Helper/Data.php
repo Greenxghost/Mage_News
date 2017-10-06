718a6b8f0a907f6f6270a886d406ae171545809b
Greenghost <ghosty.dev@gmail.com>
2017-10-06 14:24:50 +0200

    Basic Structure 0.1.0

---
+++ project/app/code/local/Iacuzio/News/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Iacuzio News
+ */
+
+/**
+ * Iacuzio News Helper Data
+ *
+ * Main Helper
+ * @author Francesco Iacuzio <ghosty.dev@gmail.com>
+ * @version 0.1.0
+ * @package Cms
+ */
+class Iacuzio_News_Helper_data extends Mage_Core_Helper_Abstract
+{
+
+}
